11914dfc40973eb12463ee2204556130a4a4241a
Merlin <merlin.regalado@dtsmine.pe>
2022-12-23 11:30:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from './Canvas'
+import { ClearCanvasButton } from './ClearCanvasButton';
+
+function App() {
+  return (
+    <>
+        <ClearCanvasButton/>
+        <Canvas/>
+    </>
+  );
+}
+
+export default App;
